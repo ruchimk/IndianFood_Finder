6f72cc3e44965755082630ee82433622cce7dd6b
Ruchi Malik <rmalik2788@gmail.com>
2014-12-29 20:37:04 -0500

    playing with search in home controller to search around user's location

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,7 @@ class HomeController < ApplicationController
   def search
     # pry -r ./config/environment (to use PRY instead of IRB as REPL)
     params = { term: "indian restaurants", limit: 16}
-    coordinates = { latitude: 37.7577, longitude: -122.4376 }
-    # coordinates = { latitude: params[:lat], longitude: params[:lng] }
+    coordinates = { latitude: 37.7577, longitude: -122.4376 } || { latitude: params[:lat], longitude: params[:lng] }
     locale = { lang: 'en' }
     render json: Yelp.client.search_by_coordinates(coordinates, params, locale)
   end
